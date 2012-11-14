942c8dc3b1ba2a079bb1b374becacb8a26ef614c
Frits Kruis <fkruis@yahoo.com>
2012-11-14 14:13:04 -0600

    Initial commit

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/media/Data/Projects/jEdit/BatJuice/bin/AndroidManifest.xml : \
+/media/Data/Projects/jEdit/BatJuice/AndroidManifest.xml \
---
+++ bin/BatJuice.ap_.d
@@ -0,0 +1,4 @@
+/media/Data/Projects/jEdit/BatJuice/bin/BatJuice.ap_ \
+ : /media/Data/Projects/jEdit/BatJuice/res/layout/main.xml \
+/media/Data/Projects/jEdit/BatJuice/res/values/strings.xml \
+/media/Data/Projects/jEdit/BatJuice/bin/AndroidManifest.xml \
